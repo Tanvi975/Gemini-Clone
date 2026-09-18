54ca5877121076cde90c1066b9d9ea4960cc530f
Tanvi975 <tanvi87917@gmail.com>
2026-09-18 14:20:42 +0530

    feat: Connect InputBox and ChatWindow to Context

--- src/components/ChatWindow.jsx
+++ src/components/ChatWindow.jsx
@@ -1,15 +1,14 @@
 import MessageBubble from './MessageBubble'
 import TypingIndicator from "./TypingIndicator"
+import { useChat } from '../context/ChatContext'
+
 
 function ChatWindow() {
-  const dummyMessages = [
-    { text: "Hi, how are you?", sender: "user" },
-    { text: "I'm doing great! How can I help you today?", sender: "ai" },
-  ]
+  const { messages } = useChat()
 
   return (
     <div className="flex-1 overflow-y-auto px-6 py-4 flex flex-col gap-4">
-      {dummyMessages.map((msg, index) => (
+      {messages.map((msg, index) => (
         <MessageBubble key={index} text={msg.text} sender={msg.sender} />
       ))}
       <TypingIndicator />
--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,8 +1,18 @@
 import { useState } from 'react'
 import { SendHorizontal } from 'lucide-react'
+import { useChat } from '../context/ChatContext'
 
 function InputBox() {
   const [text, setText] = useState("")
+  const { messages, setMessages } = useChat()
+
+  const handleSend = () => {
+    if (text.trim() === "") return
+
+    const newMessage = { text: text, sender: "user" }
+    setMessages([...messages, newMessage])
+    setText("")
+  }
 
   return (
     <div className="flex items-center bg-gray-100 rounded-full px-4 py-2 w-full max-w-2xl mx-auto">
@@ -10,10 +20,14 @@ function InputBox() {
       type="text"
       value={text}
       onChange={(e) => setText(e.target.value)}
+      onKeyDown={(e) => {
+        if (e.key === "Enter") handleSend()
+      }}
       placeholder="Ask Gemini"
       className="flex-1 bg-transparent outline-none px-2"
     />
-    <button className="bg-black text-white rounded-full w-8 h-8 flex items-center justify-center">
+    <button onClick={handleSend} className="bg-black text-white rounded-full w-8 h-8 flex items-center justify-center">
+    
     <SendHorizontal size={18} />
     </button>
     </div>
