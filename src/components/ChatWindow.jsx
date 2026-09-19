e883a9100582befb4315333da223b4463ee1eab2
Tanvi975 <tanvi87917@gmail.com>
2026-09-19 09:06:12 +0530

    fix: add mic and plus icon

--- src/components/ChatWindow.jsx
+++ src/components/ChatWindow.jsx
@@ -1,10 +1,16 @@
 import MessageBubble from './MessageBubble'
 import TypingIndicator from "./TypingIndicator"
 import { useChat } from '../context/ChatContext'
+import { useEffect, useRef } from 'react'
 
 
 function ChatWindow() {
   const { messages, loading } = useChat()
+  const bottomRef = useRef(null)
+
+  useEffect(() => {
+    bottomRef.current?.scrollIntoView({ behavior: "smooth" })
+  }, [messages, loading])
 
   return (
     <div className="flex-1 overflow-y-auto px-6 py-4 flex flex-col gap-4">
@@ -12,6 +18,7 @@ function ChatWindow() {
         <MessageBubble key={index} text={msg.text} sender={msg.sender} />
       ))}
       {loading && <TypingIndicator />}
+      <div ref={bottomRef} />
     </div>
   )
 }
--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -2,6 +2,7 @@ import { askGemini } from '../api'
 import { useState } from 'react'
 import { LuCircleArrowUp } from "react-icons/lu";
 import { useChat } from '../context/ChatContext'
+import { FaPlus, FaMicrophone } from "react-icons/fa"
 
 function InputBox() {
   const [text, setText] = useState("")
@@ -25,20 +26,24 @@ function InputBox() {
 
   return (
     <div className={`flex items-center rounded-full px-4 py-2 w-full max-w-2xl mx-auto ${isDark ? "bg-[#1e1f20]" : "bg-white border border-gray-300"}`}>
-    <input
-      type="text"
-      value={text}
-      onChange={(e) => setText(e.target.value)}
-      onKeyDown={(e) => {
-        if (e.key === "Enter") handleSend()
-      }}
-      placeholder="Ask Gemini"
-      className={`flex-1 bg-transparent outline-none px-2 ${isDark ? "text-white placeholder-gray-400" : "text-gray-900 placeholder-gray-500"}`}
-    />
-    <button onClick={handleSend} className={`rounded-full w-8 h-8 flex items-center justify-center ${isDark ? "bg-white text-black" : "bg-gray-900 text-white"}`}>
-    
-    <LuCircleArrowUp size={18} />
-    </button>
+      <FaPlus className={isDark ? "text-gray-400" : "text-gray-500"} size={16} />
+  
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") handleSend()
+        }}
+        placeholder="Ask Gemini"
+        className={`flex-1 bg-transparent outline-none px-2 ${isDark ? "text-white placeholder-gray-400" : "text-gray-900 placeholder-gray-500"}`}
+      />
+  
+      <button onClick={handleSend} className={`rounded-full w-8 h-8 flex items-center justify-center mr-2 ${isDark ? "bg-white text-black" : "bg-gray-900 text-white"}`}>
+        <LuCircleArrowUp size={18} />
+      </button>
+  
+      <FaMicrophone className={isDark ? "text-gray-400" : "text-gray-500"} size={16} />
     </div>
   )
 }
